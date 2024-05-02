9d64e1d07d8ab904b744d3cf237cedaf79d0d61c
Douglas França <douglasbragaf@gmail.com>
2024-05-02 13:52:48 -0300

    adicionando produto no carrinho - localStorage

--- src/js/components/produtos.js
+++ src/js/components/produtos.js
@@ -1,12 +1,24 @@
+function addToCart(produtoNome, produtoPreco, produtoImagem) {
+  const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+  cart.push({ name: produtoNome, price: produtoPreco, image: produtoImagem });
+  localStorage.setItem('cart', JSON.stringify(cart));
+  alert(`${produtoNome} adicionado ao carrinho!`);
+}
+
 function setupCardInteractions() {
   const cards = document.querySelectorAll('.card');
   cards.forEach(card => {
-      const saibaMaisButton = card.querySelector('button');
-      saibaMaisButton.addEventListener('click', () => {
-          alert(`Saiba mais sobre: ${card.querySelector('h3').textContent}`);
-      });
+    const saibaMaisButton = card.querySelector('button');
+    const produtoNome = card.querySelector('h3').textContent;
+    const produtoPreco = card.querySelector('p').textContent;
+    const produtoImagem = card.querySelector('img').src;
+
+    saibaMaisButton.addEventListener('click', () => {
+      addToCart(produtoNome, produtoPreco, produtoImagem);
+    });
   });
 }
+
 
 // Função para renderizar os produtos no DOM
 export function renderProducts(produtos) {
@@ -20,7 +32,7 @@ export function renderProducts(produtos) {
       <img src="${produto.foto}" alt="${produto.nome}">
       <h3>${produto.nome}</h3>
       <p>${produto.preco}</p>
-      <button>Saiba mais</button>
+      <button>Adicionar ao Carrinho</button>
     `;
     productsContainer.appendChild(card);
   });
